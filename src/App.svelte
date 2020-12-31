f25be93a00fadfff6ab72b55bd820470910a2fa2
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-01-01 02:25:32 +0700

    feat: create login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,54 @@
 <script lang="ts">
-  export let appName;
+  import { Router, Route } from 'svelte-routing';
+  import Redirect from './components/routers/Redirect.svelte';
+  import Login from './views/Login.svelte';
 </script>
 
-<main>
-  <h1 class="font-bold text-3xl">{appName}</h1>
-</main>
-
 <style global lang="postcss">
-
   /* only apply purgecss on utilities, per Tailwind docs */
   /* purgecss start ignore */
   @tailwind base;
+
   * {
-    @apply box-border font-base;
+    @apply box-border font-base text-dark;
+  }
+
+  h1 {
+    @apply text-3xl font-bold;
+  }
+
+  caption {
+    @apply text-xs text-accent-one font-medium;
+  }
+
+  @screen md {
+    h1 {
+      @apply text-4xl;
+    }
+
+    caption {
+      @apply text-sm;
+    }
   }
 
   @tailwind components;
+
+  .row {
+    @apply flex flex-row;
+  }
+
+  .col {
+    @apply flex flex-col;
+  }
+
   /* purgecss end ignore */
 
   @tailwind utilities;
-
 </style>
+
+<main class="bg-background min-h-screen">
+  <Router url="">
+    <Route path="login" component={Login} />
+    <Redirect path="/" to="/login" />
+  </Router>
+</main>
---
+++ src/views/Login.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Button from '../components/shared/Button.svelte';
+  import Card from '../components/shared/Card.svelte';
+  import Input from '../components/shared/Input.svelte';
+  import SizedBox from '../components/shared/SizedBox.svelte';
+</script>
+
+<div class="row min-h-screen justify-center items-center">
+  <Card center>
+    <h1>Login</h1>
+    <SizedBox height={2} />
+    <div class="row">
+      <caption>Don't have an account?&nbsp;</caption>
+      <div class="text-xs md:text-sm text-primary font-medium underline">
+        Sign up
+      </div>
+    </div>
+    <SizedBox height={8} />
+    <Input placeholder="Username" />
+    <SizedBox height={6} />
+    <Input placeholder="Password" />
+    <SizedBox height={10} />
+    <Button text="Sign In" primary />
+  </Card>
+</div>
