db6a501fd386f75c170e8bead7ba5d8eb591f958
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-12-31 23:17:22 +0700

    feat: create shared card component

---
+++ src/components/shared/Card.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let center = false;
+  export let className = "";
+</script>
+
+<style scoped lang="postcss">
+  .card {
+    @apply w-1/3 px-8 py-4 rounded-card bg-white shadow-card;
+  }
+
+  .center {
+    @apply items-center;
+  }
+</style>
+
+<div class:center class={`card col ${className}`}>
+  <slot />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 // tailwind.config.js
 const production = !process.env.ROLLUP_WATCH; // or some other env var like NODE_ENV
 module.exports = {
-  future: { // for tailwind 2.0 compat
-    purgeLayersByDefault: true, 
+  future: {
+    // for tailwind 2.0 compat
+    purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
   plugins: [
@@ -10,14 +11,32 @@ module.exports = {
   ],
   purge: {
     content: [
-      "./src/**/*.svelte",
+      './src/**/*.svelte',
       // may also want to include base index.html
-    ], 
-    enabled: production // disable purge in dev
+    ],
+    enabled: production, // disable purge in dev
   },
   theme: {
     fontFamily: {
-      base: ['Work Sans']
-    }
-  }
+      base: ['Work Sans'],
+    },
+    colors: {
+      background: '#E7EEF2',
+      primary: '#4792BB',
+      primaryDark: '#4792BB',
+      accent: '#879098',
+      dark: '#273038',
+      white: '#F4F8FA',
+      secondary: '#EEEEEE',
+    },
+    boxShadow: {
+      card: '0 16px 100px rgba(0, 0, 0, .1)',
+      input: '0 0 4px rgba(0, 0, 0, .1)',
+      'input-active': '0 0 8px 2px rgba(0, 0, 0, .1)',
+    },
+    borderRadius: {
+      card: '16px',
+      input: '8px',
+    },
+  },
 };
