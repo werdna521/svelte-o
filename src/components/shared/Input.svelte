52b19e9344673b087d2e03951015916159865f4a
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-12-31 23:30:30 +0700

    feat: create input component

---
+++ src/components/shared/Input.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  export let value: string;
+  export let className = "";
+</script>
+
+<style scoped lang="postcss">
+  input {
+    @apply px-4 py-2 w-full rounded-input shadow-input bg-secondary;
+    @apply text-base font-medium border-secondary border-2 transition-all;
+  }
+
+  input:focus {
+    @apply bg-white shadow-input-active outline-none;
+  }
+
+  ::placeholder {
+    @apply text-base text-accent font-medium;
+  }
+</style>
+
+<input class={className} bind:value placeholder="hello" />
