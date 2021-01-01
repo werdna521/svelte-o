85ee70bb59a2e6a8d77089cfc20c9b6f10919654
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-01-01 21:06:52 +0700

    feat: create scrollview

---
+++ src/components/shared/ScrollView.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import convert from '../../utils/convert';
+
+  export let offset = 0;
+
+  $: off = convert.tailwindToRem(offset);
+</script>
+
+<style scoped lang="postcss">
+  .list-container {
+    @apply flex-grow overflow-y-scroll;
+  }
+</style>
+
+<div class="list-container col" style="max-height: calc(100vh - {off}rem);">
+  <slot />
+</div>
