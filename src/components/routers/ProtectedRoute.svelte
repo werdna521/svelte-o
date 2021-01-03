47cc9a1f86832b5c60d2a7f0f473828c1b7c63a1
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-01-03 22:20:27 +0700

    feat: create protected route

---
+++ src/components/routers/ProtectedRoute.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { navigate } from 'svelte-routing';
+  import firebase from '../../firebase';
+
+  onMount(() => {
+    if (!firebase.authenticated()) {
+      navigate('/login');
+    }
+  });
+</script>
+
+<slot />
