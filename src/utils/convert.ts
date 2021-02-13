755ba2e0dcae11d65bbd286777dcb4ce4560fac8
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-02-13 22:11:10 +0700

    feat: add html strip module

--- src/utils/convert.ts
+++ src/utils/convert.ts
@@ -2,4 +2,9 @@ export default {
   tailwindToRem(val: number): number {
     return val * 0.25;
   },
+  stripHtml(html: string) {
+    const tmp = document.createElement('DIV');
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText || '';
+  },
 };
