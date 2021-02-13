40487f8ce90554c241f528ff4947dada5f5f0c5d
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2021-02-13 22:12:02 +0700

    fix: fix protected route bug

--- src/components/routers/ProtectedRoute.svelte
+++ src/components/routers/ProtectedRoute.svelte
@@ -1,13 +1,10 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
   import { navigate } from 'svelte-routing';
   import firebase from '../../firebase';
 
-  onMount(() => {
-    if (!firebase.authenticated()) {
-      navigate('/');
-    }
-  });
+  if (!firebase.authenticated()) {
+    navigate('/');
+  }
 </script>
 
 <slot />
